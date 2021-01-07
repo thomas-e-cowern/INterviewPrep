9fe38bb1fcf40d5bfb252be9a76ff8033ef5bea9
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-01-07 12:32:53 -0500

    added palindrome

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -2,6 +2,32 @@ const { showCompletionScript } = require("yargs");
 
 function palindrome (str) {
   console.log(str);
+
+  let reversedStrArr = [];
+  let reversedStr = "";
+  
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    
+    reversedStrArr.unshift(element)
+
+  }
+
+  for (let j = 0; j < reversedStrArr.length; j++) {
+    const element = reversedStrArr[j];
+    
+    reversedStr = reversedStr + element;
+  }
+
+  console.log(reversedStrArr);
+  console.log(reversedStr);
+
+  if (str === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
-palindrome ('this is my string');
+console.log(palindrome ('A Man, A Plan, A Canal-Panama'));
